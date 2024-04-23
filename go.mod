2e864c06799817d5d97b340f60a03b74c0e14351
Francesco Farina <rockerg991@gmail.com>
2024-04-23 11:19:20 +0100

    Update AWS dependencies 23-04-2024 (#127)
    * Update AWS dependencies
    
    - Update lint-test.yml to use the latest version of golangci/golangci-lint-action and actions/setup-go
    - Update go.mod to use the latest version of github.com/aws/aws-sdk-go
    
    * removed v1 dep from test

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/indiependente/s3stream/v2
 go 1.12
 
 require (
-	github.com/aws/aws-sdk-go v1.44.210
-	github.com/indiependente/s3stream v1.0.0
+	github.com/aws/aws-sdk-go v1.51.26
 	github.com/pkg/errors v0.9.1
-	golang.org/x/net v0.7.0 // indirect
 )
--- s3_test.go
+++ s3_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/indiependente/s3stream"
+	"github.com/indiependente/s3stream/v2"
 )
 
 func TestStore_Get(t *testing.T) {
